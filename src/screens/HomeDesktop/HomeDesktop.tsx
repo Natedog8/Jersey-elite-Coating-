6cd9ca45c799e0c86e9ad56d379bb8e6ec1ba7eb
Natedog8 <natebarrett8@gmail.com>
2025-08-14 18:44:17 -0400

    Update HomeDesktop.tsx

--- src/screens/HomeDesktop/HomeDesktop.tsx
+++ src/screens/HomeDesktop/HomeDesktop.tsx
@@ -1,24 +1,11 @@
-import React from "react";
-import { ClientFeedbackSection } from "./sections/ClientFeedbackSection/ClientFeedbackSection";
-import { ContactSection } from "./sections/ContactSection/ContactSection";
-import { FooterSection } from "./sections/FooterSection/FooterSection";
-import { HeaderSection } from "./sections/HeaderSection/HeaderSection";
-import { HeroSection } from "./sections/HeroSection/HeroSection";
-import { MainContentSection } from "./sections/MainContentSection/MainContentSection";
-import { ServicesSection } from "./sections/ServicesSection/ServicesSection";
-import { TestimonialSection } from "./sections/TestimonialSection/TestimonialSection";
+'use client';
+import React from 'react';
 
-export const HomeDesktop = (): JSX.Element => {
-  return (
-    <div className="flex flex-col items-start relative" data-model-id="9203:61544">
-      <HeroSection />
-      <HeaderSection />
-      <ServicesSection />
-      <ContactSection />
-      <TestimonialSection />
-      <ClientFeedbackSection />
-      <MainContentSection />
-      <FooterSection />
-    </div>
-  );
-};
+export const HomeDesktop = () => (
+  <div style={{ padding: 24, fontFamily: 'sans-serif' }}>
+    Jersey Elite Coatings — Home
+  </div>
+);
+
+export default HomeDesktop;
+
